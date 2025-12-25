4d9c7b63d5b07dcab8cf842be24740258b88cf96
RaviGahire <ravigahire3@gmail.com>
2025-12-25 16:53:20 +0530

    File upload done

--- Practice/Node Exp DB/CRUD_WITH_FILE/config/connectDB.js
+++ Practice/Node Exp DB/CRUD_WITH_FILE/config/connectDB.js
@@ -4,12 +4,13 @@ const DATABASE_URL = process.env.DATABASE_URL
 
 const connectDB = async () => {
     try {
-        const database = await mongoose.connect(DATABASE_URL)
-           console.log('Database connected successfully');
+           await mongoose.connect(DATABASE_URL)
+           console.log('Database connected successfully',mongoose.connection.readyState);
 
 
     } catch (error) {
       console.error('Database connection failed:', error.message);
+      console.log(mongoose.connection.readyState)
 
     }
 }
--- Practice/Node Exp DB/CRUD_WITH_FILE/controller/Savemovie.js
+++ Practice/Node Exp DB/CRUD_WITH_FILE/controller/Savemovie.js
@@ -5,8 +5,12 @@ const movieSchema = require('../model/movieSchema');
 exports.Savemovie = async (req, res) => {
 
     try {
-        //   res.send(req.body)
-        const insert = new movieSchema(req.body)
+     // Destructure
+     const {movieTitle,director,genre,releaseDate,boxOfficeStatus,duration,rating,synopsis} = req.body;
+     const posterUpload = req.file ?  req.file.filename : 'Poster not avilable ';
+
+        const insert = new movieSchema({movieTitle,director,genre,releaseDate,boxOfficeStatus,duration,rating,synopsis,posterUpload})
+
         await insert.save()
         console.log('movie saved suceessfully ')
         res.send(`<script> 
